e7d08afcfa5467bb25ccb7b811453cb74504b880
franOrellana <orellanafrancisco98@hotmail.com>
2022-08-18 17:48:52 -0300

    API Web

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-'use strict'
-const{graphql,buildSchema}=require('graphql')
-const schema=buildSchema(`
+
+"use strict";
+const { graphql, buildSchema } = require("graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const app = express();
+const port = process.env.port || 3000;
+const schema = buildSchema(`
   type Query {
-    hello: String,
-    saludo: String
+    "Retorna un saludo al mundo"
+    hello: String
   }
-`)
-
-// resolve
-
+`);
 const resolvers = {
   hello: () => {
-    return "Hola Mundo"
+    return "Hola Mundo";
   },
-  saludo: () => {
-    return "Hola a todos"
-  }
-}
-
-
-
-graphql(schema,'{ saludo }', resolvers).then((data)=>{console.log(data)})
+};
+app.use(
+  "/api",
+  graphqlHTTP({ schema: schema, rootValue: resolvers, graphiql: true })
+);
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`);
+});
